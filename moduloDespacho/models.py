62fac157f36aa7b95a943772349231dd415801b2
ph22012 <ph22012@ues.edu.sv>
2024-12-02 20:00:21 -0600

    view 1.1

--- moduloDespacho/models.py
+++ moduloDespacho/models.py
@@ -13,7 +13,7 @@ class Order(models.Model):
     id_address=models.IntegerField() #esta es FK pero no se ha creado el modelo
     id_cupon=models.IntegerField() #esta es FK pero no se ha creado el modelo
     id_employee=models.IntegerField() #esta es FK pero no se ha creado el modelo
-    id_status=models.ForeignKey(Order_status, on_delete=models.CASCADE) #esta es FK pero no se ha creado el modelo
+    id_status=models.ForeignKey(Order_status,to_field='id_status', on_delete=models.CASCADE) #esta es FK pero no se ha creado el modelo
     order_date=models.DateTimeField() 
 
     def __str__(self):
--- moduloDespacho/views.py
+++ moduloDespacho/views.py
@@ -3,10 +3,10 @@ from .models import Order
 from datetime import date
 # Create your views here.
 #logica de la vista de despachos    
-def mostrar_ordenes(request):
+"""def mostrar_ordenes(request):
     ordenesP = Order.objects.filter(order_date = fechaHoy)
 
-    return render (request,{'ordenesP':ordenesP})
+    return render (request,{'ordenesP':ordenesP})"""
 
 def fechaHoy ():
     today = date.today()
