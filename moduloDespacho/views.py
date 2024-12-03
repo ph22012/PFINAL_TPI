0588056b335345b6a6e667aa0812bd605c0063a6
Fredy-Cruz <cr22009@ues.edu.sv>
2024-12-02 18:41:38 -0600

    Prueba de pintado de la tabal orders

--- moduloDespacho/views.py
+++ moduloDespacho/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Order
 
-# Create your views here.
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request, 'moduloDespacho/Orders.html', {'orders': orders})
