ae428d0c2d8bfc878598aa44fc3f391435020f28
ph22012 <ph22012@ues.edu.sv>
2024-12-03 15:37:23 -0600

    Antes de las pruebas de channels y websocket

--- moduloDespacho/views.py
+++ moduloDespacho/views.py
@@ -3,11 +3,6 @@ from .models import Order
 from datetime import date
 # Create your views here.
 #logica de la vista de despachos    
-"""def mostrar_ordenes(request):
-    ordenesP = Order.objects.filter(order_date = fechaHoy)
-
-    return render (request,{'ordenesP':ordenesP})"""
-
 def fechaHoy ():
     today = date.today()
     return today
