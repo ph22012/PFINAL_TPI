78d18e43c97aee0caafaf52ef6abf2c0ededfa9b
carlos argueta <carlosargueta701@gmail.com>
2024-12-02 20:30:12 -0600

    version 1 modulo administracion

--- modulo_administracion/models.py
+++ modulo_administracion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Configuration(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    path_logo = models.CharField(max_length=100)
+    path_slogan = models.CharField(max_length=100)
+    color_pallette = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    isPointActive = models.BooleanField(default=False)
+
--- modulo_administracion/views.py
+++ modulo_administracion/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def GeneralView(request):
+    return render(request, 'administracion/inicio.html')
+
+
