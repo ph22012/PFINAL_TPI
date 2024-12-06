db368025df0d16553a60e33ef648176746e90674
JaimeGD09 <jaimegildiaz3838@gmail.com>
2024-12-06 15:23:39 -0600

    ola

--- modulo_menu/models.py
+++ modulo_menu/models.py
@@ -3,19 +3,31 @@ from django.db import models
 # Create your models here.
 # category, product, menu
 class Category(models.Model):
-    id_category = models.AutoField(primary_key=True, auto_created=True)
+    id_category = models.AutoField(primary_key=True)
     name = models.CharField(max_length=256)
-
+    
+    class Meta:
+        db_table = "category"
+        managed = False
+        
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
-    id_product = models.AutoField(primary_key=True, auto_created=True)
-    id_category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=256) #no esta en el modelo
+    id_product = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=256)
+    id_category = models.ForeignKey('Category',to_field='id_category',db_column='id_category', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     count = models.IntegerField()
-    dailyMenuDate = models.DateField()   
+    daily_menu_date = models.DateField(null=True, blank=True)
+    
+    class Meta:
+        db_table = "product"
+        managed = False
 
+    def __str__(self):
+        return self.name
 #class Menu(models.Model):
 
 """
