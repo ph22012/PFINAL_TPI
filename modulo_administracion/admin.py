b22f908fb4d0811bab8d2b3c00709b93cea708b4
Karen Sanchez <sp21013@ues.edu.sv>
2024-12-07 18:38:15 -0600

    fix gestión de usuarios: customers

--- modulo_administracion/admin.py
+++ modulo_administracion/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Configuration, Role, Employee, Customer, Cupon
+from .models import Configuration, RewardPoints, Role, Employee, Customer, Cupon
 
 admin.site.register(Configuration)
 admin.site.register(Role)
 admin.site.register(Employee)
 admin.site.register(Customer)
 admin.site.register(Cupon)
+admin.site.register(RewardPoints)
