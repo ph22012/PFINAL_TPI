232769b39f86b4d57ff32a3a40632eed7872a418
Fredy-Cruz <cr22009@ues.edu.sv>
2024-12-08 00:10:50 -0600

    Fix: PWA de Repartidores arreglada

--- PROYECTO_FINAL_TPI/settings.py
+++ PROYECTO_FINAL_TPI/settings.py
@@ -166,17 +166,20 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
 	{
-		
+		'src': 'static/images/icon.png',
+		'sizes': '160x160'
 	}
 ]
 PWA_APP_ICONS_APPLE = [
 	{
-		
+		'src': 'static/images/icon.png',
+		'sizes': '160x160'
 	}
 ]
 PWA_APP_SPLASH_SCREEN = [
 	{
-		
+		'src': 'static/images/icon.png',
+		'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
 	}
 ]
 PWA_APP_DIR = 'ltr'
