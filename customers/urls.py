63528fd3b61af289668f71b17cc1792bbe594c42
JaimeGD09 <jaimegildiaz3838@gmail.com>
2024-12-08 21:05:00 -0600

    fin

--- customers/urls.py
+++ customers/urls.py
@@ -6,11 +6,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('profile/', views.profile, name='profile'),
     path('principal/', views.principal, name='principal'),
-<<<<<<< HEAD
-    path('logout/', views.custom_logout, name='logout'),
-=======
     path('productos/', views.productos, name='productos'),
     path('carrito/', views.carrito, name='carrito'),
     path('logout/', views.logout, name='logout'),
->>>>>>> 4165b2048be9b1870e0e1e3ca6611d8f419a2017
 ]
